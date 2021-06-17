c162d76f2ac6c5cb64de165ed936e12843ac93a0
baofu <bf.ge@zuche.com>
2021-06-17 10:52:42 +0800

    allow all ip access

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hg2moy#)cza2^sckb79l(lgfcm70amq5@))l&q)8$)*re92^x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
