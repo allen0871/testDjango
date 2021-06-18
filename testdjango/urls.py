61bdd199ea3af396f5e376ba1f7a67bf16ca93e6
root <root@happy-ponies-3.localdomain>
2021-06-18 02:39:20 -0400

    fix static error

--- testdjango/urls.py
+++ testdjango/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf.urls import url,include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hotnews/', views.get_BaiduHotNews),
     url(r'^$', views.hello),
-]
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
